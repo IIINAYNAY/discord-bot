b0a79d4a02c513c57f339c858eb8143e2c075587
IIINAYNAY <97263275+IIINAYNAY@users.noreply.github.com>
2022-01-06 18:26:18 -0600

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.on('ready', () => {
+console.log('Your Bot is Up and running Nick!!!')
+});
+
+
+
+// this has to be at the bottom always
+client.login('OTI4ODA1MDgwMzAyNDg5NjAx.YdeHIw.io4rQ5mjaEtFgjtzcMkfiVQUyZg');
